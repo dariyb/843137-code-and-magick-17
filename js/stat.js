71f7b6be47ebf4fbec6abb5882c06ef5dc0cd605
dariy <gomzykovad@gmail.com>
2019-06-06 20:17:42 +1000

    гистограмма

--- js/stat.js
+++ js/stat.js
@@ -2,30 +2,42 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var COLUMN_WIDTH = 40;
+var TEXT_Y = 265;
+var COLUMN_X = 100;
+var GAP = 50;
+var HISTO_HEIGHT = 150;
 
-var renderCloud = function (ctx,x,y,a,b,CLOUD_WIDTH,d,CLOUD_WIDTH,f,g,CLOUD_HEIGHT,t,CLOUD_HEIGHT,p,s,z,q,l,m,color ) {
+var renderCloud = function (ctx,x,y,color) {
   ctx.fillStyle = color;
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(a, b);
-  ctx.lineTo(CLOUD_WIDTH, d);
-  ctx.lineTo(CLOUD_WIDTH, f);
-  ctx.lineTo(g, CLOUD_HEIGHT);
-  ctx.lineTo(t, CLOUD_HEIGHT);
-  ctx.lineTo(p, s);
-  ctx.lineTo(z, q);
-  ctx.lineTo(l, m);
-  ctx.closePath();
-  ctx.stroke();
-  ctx.fill();
+  ctx.fillRect(x,y,CLOUD_WIDTH,CLOUD_HEIGHT);
+};
+
+var getMaxTime = function (array) {
+  var maxElement = array[0];
+  if (array[i]>maxElement) {
+    maxElement = array[i];
+  }
+  return maxElement;
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, 110, 20,330, 20, 430, 110, 430, 210, 330, 280, 110, 280, 10, 210, 10, 110, 110, 20, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, 100, 10, 320, 10, CLOUD_WIDTH, 100, CLOUD_WIDTH, 200, 320, CLOUD_HEIGHT, 100, CLOUD_HEIGHT, 0, 200, 0, 100, 100, 10, '#fff');
+  renderCloud(ctx,110,20,'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx,100,10,'#fff');
+  ctx.fillStyle = '#000000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура! Вы победили!', 110, 30);
+  ctx.fillText('Список результатов:', 110, 50);
   ctx.fillStyle = '#000';
-  ctx.fillText('Вы', 100, 250);
-  ctx.fillRect(100, 100, 40, 150);
-  ctx.fillText('Юлия', 110, 135);
-  ctx.fillRect(190, 120, 40, 100);
+
+  var maxTime = getMaxTime(times);
+
+  for (var i=0;i<names.length;i++) {
+    ctx.fillText(names[i],COLUMN_X+COLUMN_WIDTH*i+GAP*(i+1),TEXT_Y);
+    ctx.fillRect(COLUMN_X+COLUMN_WIDTH*i+GAP*(i+1),100,COLUMN_WIDTH,columnHeight);
+  };
+  for (var j=0;j<times.length;j++) {
+    var nameTime = Math.round(times[i]);
+    var columnHeight = nameTime * HISTO_HEIGHT / (maxTime -0);
+  }
 };
