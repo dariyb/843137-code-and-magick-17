8bb896bca48144a0df4afbcd7ccbecddd4f9a153
dariy <gomzykovad@gmail.com>
2019-06-06 23:41:48 +1000

    homework2

--- js/stat.js
+++ js/stat.js
@@ -8,16 +8,16 @@ var COLUMN_X = 100;
 var GAP = 50;
 var HISTO_HEIGHT = 150;
 var columnY = 250;
-var lineHeight=20;
+var lineHeight = 20;
 
-var renderCloud = function (ctx,x,y,color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x,y,CLOUD_WIDTH,CLOUD_HEIGHT);
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx,110,20,'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx,100,10,'#fff');
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#fff');
   ctx.fillStyle = '#000000';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура! Вы победили!', 110, 45);
@@ -26,9 +26,9 @@ window.renderStatistics = function (ctx, names, times) {
 
   var getMaxTime = function (array) {
     var maxElement = -1;
-    for (var i=0;i<array.length;i++) {
-      if (array[i]>maxElement) {
-        maxElement=array[i];
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] > maxElement) {
+        maxElement = array[i];
       }
     }
     return maxElement;
@@ -38,22 +38,22 @@ window.renderStatistics = function (ctx, names, times) {
     if (playerName === 'Вы') {
       return 'rgba(255, 0, 0, 1)';
     }
-    return 'rgba(0, 0, 255, '+Math.random()+')';
+    return 'rgba(0, 0, 255, ' + Math.random() + ')';
   };
 
   var maxTime = getMaxTime(times);
 
-  for (var i=0;i<times.length;i++) {
+  for (var i = 0; i < times.length; i++) {
     var nameTime = Math.round(times[i]);
     var columnHeight = nameTime * HISTO_HEIGHT / maxTime;
     ctx.fillStyle = getNameColor(names[i]);
-    ctx.fillRect(COLUMN_X+COLUMN_WIDTH*i+GAP*(i+1),columnY,COLUMN_WIDTH,columnHeight*(-1));
+    ctx.fillRect(COLUMN_X + COLUMN_WIDTH * i + GAP * (i + 1), columnY, COLUMN_WIDTH, columnHeight * (-1));
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i],COLUMN_X+COLUMN_WIDTH*i+GAP*(i+1),TEXT_Y);
-    ctx.fillText(nameTime,COLUMN_X+COLUMN_WIDTH*i+GAP*(i+1),columnY-columnHeight-lineHeight/2);
-  };
+    ctx.fillText(names[i], COLUMN_X + COLUMN_WIDTH * i + GAP * (i + 1), TEXT_Y);
+    ctx.fillText(nameTime, COLUMN_X + COLUMN_WIDTH * i + GAP * (i + 1), columnY - columnHeight - lineHeight / 2);
+  }
 
-  for (var i=0;i<names.length;i++) {
-    ctx.fillText(names[i],COLUMN_X+COLUMN_WIDTH*i+GAP*(i+1),TEXT_Y);
-  };
+  for (i = 0; i < names.length; i++) {
+    ctx.fillText(names[i], COLUMN_X + COLUMN_WIDTH * i + GAP * (i + 1), TEXT_Y);
+  }
 };
