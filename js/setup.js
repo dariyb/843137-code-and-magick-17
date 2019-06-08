e59f8c25f8fb25383e77f0c1c27ed74c4f9584b7
dariy <gomzykovad@gmail.com>
2019-06-08 19:32:22 +1000

    rn

--- js/setup.js
+++ js/setup.js
@@ -26,7 +26,7 @@ var randomEyes = Math.floor(Math.random() * eyesColor.length);
 
 var wizards = [
   {
-    name: getRandomNumbers(wizardsNames) + '\n' + wizardsLastNames[randomLastName],
+    name: wizardsNames[getRandomNumbers()] + '\n' + wizardsLastNames[randomLastName],
     coatColor: coatColor[randomCoat],
     eyesColor: eyesColor[randomEyes]
   },
