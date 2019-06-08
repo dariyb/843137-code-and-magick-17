1c7139a82530eaa602a37ea44e936a59747195d1
dariy <gomzykovad@gmail.com>
2019-06-08 18:16:46 +1000

    re

--- js/setup.js
+++ js/setup.js
@@ -6,30 +6,16 @@ var wizardsNames = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var wizardsLastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
-for (var i = 0; i <wizardsNames.length; i++) {
+for (var j = 0; j < wizardsNames.length; j++) {
   var randomIndex = Math.floor(Math.random * wizardsNames.length);
   var randomElement = wizardsNames[randomIndex];
 }
 var wizards = [
   {
     name: randomElement,
-    coatColor: ,
-    eyesColor:
   },
   {
     name: randomElement,
-    coatColor: ,
-    eyesColor:
-  },
-  {
-    name: ,
-    coatColor: ,
-    eyesColor:
-  },
-  {
-    name: ,
-    coatColor: ,
-    eyesColor:
   }
 ];
 var similarListElement = document.querySelector('.setup-similar-list');
