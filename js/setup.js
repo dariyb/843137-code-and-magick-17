b5fe5c15b184a743b71f2b51dea07b0d6d4c330f
dariy <gomzykovad@gmail.com>
2019-06-08 17:39:12 +1000

    random name

--- js/setup.js
+++ js/setup.js
@@ -15,5 +15,6 @@ var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 for (var i = 0; i < 4; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
   similarListElement.appendChild(wizardElement);
 }
