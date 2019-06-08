c3197d67b4218d0a5f91af2c2a411d776b8fc105
dariy <gomzykovad@gmail.com>
2019-06-08 17:30:17 +1000

    similar setup

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,7 @@
 'use strict';
 var setupBlock = document.querySelector('.setup');
 setupBlock.classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
 var wizardsNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var wizardsLastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var randomName = Math.floor(Math.random() * wizardsNames.length);
