203e2f409345aec9e78a2418f1fe38e6ca834be2
dariy <gomzykovad@gmail.com>
2019-06-08 18:18:12 +1000

    re

--- js/setup.js
+++ js/setup.js
@@ -6,7 +6,7 @@ var wizardsNames = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var wizardsLastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
-for (var j = 0; j < wizardsNames.length; j++) {
+for (var i = 0; i < wizardsNames.length; i++) {
   var randomIndex = Math.floor(Math.random * wizardsNames.length);
   var randomElement = wizardsNames[randomIndex];
 }
@@ -16,11 +16,17 @@ var wizards = [
   },
   {
     name: randomElement,
+  },
+  {
+    name: randomElement,
+  },
+  {
+    name: randomElement,
   }
 ];
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-for (var i = 0; i < 4; i++) {
+for (i = 0; i < 4; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
   similarListElement.appendChild(wizardElement);
