695a8d01e378fdfa05ad399d70c9d22fe06f5fb1
dariy <gomzykovad@gmail.com>
2019-06-08 17:27:33 +1000

    begining

---
+++ js/setup.js
@@ -0,0 +1,12 @@
+'use strict';
+var setupBlock = document.querySelector('.setup');
+setupBlock.classList.remove('hidden');
+var wizardsNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var wizardsLastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var randomName = Math.floor(Math.random() * wizardsNames.length);
+var randomLastName = Math.floor(Math.random() * wizardsLastNames);
+var wizards = [
+  {
+    name: wizardsNames[randomName] + wizardsLastNames[randomLastName]
+  }
+];
